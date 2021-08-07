42edba4e875124c58ab517ff2fba125b665304d3
sanketshirsath226 <65910774+sanketshirsath226@users.noreply.github.com>
2021-08-07 15:57:32 +0530

    Rename Main.py to main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from pytube import YouTube
+yt = YouTube('http://youtube.com/watch?v=9bZkp7q19f0')
+result = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
